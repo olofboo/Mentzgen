7600abb326b03c415930bd6c071d4ed58eb82d2b
Olli Björkqvist <olof@crunchbang.Elisa>
2016-06-08 15:24:59 +0300

    annotate and rdoc

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: characters
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  str        :integer
+#  int        :integer
+#  wis        :integer
+#  con        :integer
+#  dex        :integer
+#  cha        :integer
+#  hp         :integer
+#  level      :integer
+#  xp         :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  alignment  :string
+#
+
 require 'test_helper'
 
 class CharacterTest < ActiveSupport::TestCase
