51dd7a312a7ad5fae3da949e26e6e24eeb444805
Olli Björkqvist <olof@crunchbang.Elisa>
2016-06-09 13:51:12 +0300

    added dice helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :dice
+
+  def dice(amount, size)
+        result = 0
+        amount.times do
+                result = result + Random.new.rand(1..size)
+        end
+  return result
+  end
+
 end
